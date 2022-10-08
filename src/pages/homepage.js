734ab364c1838b1a865724277706e04c2bf34041
Ayub <kayub4947@gmail.com>
2022-10-08 09:40:33 +0530

    carosel

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,11 +1,54 @@
-import React from 'react';
-  
-const Home = () => {
+import Carousel from 'react-bootstrap/Carousel';
+import img1 from "../assets/index1.jpeg";
+import img2 from "../assets/index2.jpeg";
+import img3 from "../assets/index3.jpeg";
+import LiveRoom from './live_room';
+import Audio_Book from './audiobook';
+
+function UncontrolledExample() {
   return (
-    <div className="bg-dark">
-      <h1 className='text-light'>This is HomePage </h1>
-    </div>
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block"
+          src={img1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block"
+          src={img2}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block "
+          src={img3}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    
+
   );
-};
-  
-export default Home;
+}
+
+export default UncontrolledExample;
